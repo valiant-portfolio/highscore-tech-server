0c35a9b93533cd759cfa2be3c6872dc6a8008d58
Highscoretech <highscoretechng@gmail.com>
2025-08-19 16:52:46 +0100

    first commit

--- services/mongo/index.js
+++ services/mongo/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const mongodbString = require("../../config/mongo.confiq.js")
 
 mongoose.set('strictQuery', false);
-const dbUri = mongodbString.developement
+const dbUri = mongodbString.production
  
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000  })
 .then(async (result) => {
