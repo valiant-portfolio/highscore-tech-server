524d423fd37becdff70e051b2aa5b42697fc49b6
Highscoretech <highscoretechng@gmail.com>
2025-09-01 11:28:11 +0100

    ko

--- routes/route.manager.js
+++ routes/route.manager.js
@@ -1,8 +1,10 @@
 const authRoute = require("./auth/auth.route")
+const Blog = require("./auth/auth.route")
 
 const routeManager = (app) => {
     // API Routes
     app.use("/auth", authRoute);
+    app.use("/api", Blog);
 
 }
 
